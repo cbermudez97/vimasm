5801356ce10522b210c259ee5cdc2eb9bd16aee4
Carlos Bermudez Porto <cbermu.97@gmail.com>
2017-11-20 13:03:02 -0500

    Adding the Get_Ascii methods and Ascii arrays

--- src/keyboard.asm
+++ src/keyboard.asm
@@ -2,8 +2,15 @@ section .data
 
 ; Previous scancode.
 key db 0
+ASCII_CODE_S db  0,27,49,50,51,52,53,54,55,56,57,48,45,61,8,9,113,119,101,114,116,121,117,105,111,112,91 ,93 ,13,0,97,115,100,102,103,104,106,107,108,59,39,96 ,0,92 ,122,120,99,118,98,110,109,44,46,47,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
+ASCII_CODE_NS db 0,27,33,64,35,36,37,94,38,42,40,41,95,43,8,9,81 ,87 ,69 ,82 ,84 ,89 ,85 ,73 ,79 ,80 ,123,125,13,0,65,83 ,68 ,70 ,71 ,72 ,74 ,75 ,76 ,58,34,126,0,124,90 ,88 ,67,86 ,66,78 ,77 ,60,62,63,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
+ASCII_CODE_LEN dd 83 
 
 section .text
+;parametro 2 registro donde guardar, parametro 1 codigo hex de la tecla 
+%macro GET_ASCII 2.nolist
+mov %2, [ASCII_CODE + %1]
+%endmacro
 
 ; scan()
 ; Scan for new keypress. Returns new scancode if changed since last call, zero
