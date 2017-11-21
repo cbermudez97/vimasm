ec11b8163a6cf86a59a148403ec2c8862fa89f97
Carlos Bermudez Porto <cbermu.97@gmail.com>
2017-11-21 12:02:38 -0500

    Modified adv_cursor method.

--- src/game.asm
+++ src/game.asm
@@ -3,7 +3,7 @@
 section .data
 ;Data related to the hex-ascii translate.
 ASCII_CODE_NS db  0,27,49,50,51,52,53,54,55,56,57,48,45,61,8,9,113,119,101,114,116,121,117,105,111,112,91 ,93 ,13,0,97,115,100,102,103,104,106,107,108,59,39,96 ,0,92 ,122,120,99,118,98,110,109,44,46,47,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
-ASCII_CODE_S db 0,27,33,64,35,36,37,94,38,42,40,41,95,43,8,9,81 ,87 ,69 ,82 ,84 ,89 ,85 ,73 ,79 ,80 ,123,125,13,0,65,83 ,68 ,70 ,71 ,72 ,74 ,75 ,76 ,58,34,126,0,124,90 ,88 ,67,86 ,66,78 ,77 ,60,62,63,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
+ASCII_CODE_S  db  0,27,33,64,35,36,37,94,38,42,40,41,95,43,8,9,81 ,87 ,69 ,82 ,84 ,89 ,85 ,73 ,79 ,80 ,123,125,13,0,65,83 ,68 ,70 ,71 ,72 ,74 ,75 ,76 ,58,34,126,0,124,90 ,88 ,67,86 ,66,78 ,77 ,60,62,63,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
 ASCII_CODE_LEN dd 83
 ;Data related to the text and cursor.
 TEXT times 10000000 db 0
@@ -59,6 +59,7 @@ game:
     jmp game.loop
 
 get_input:
+    push eax
     call scan; Hex code in eax(al)
     ; Check if value is valid
     cmp al, 0
@@ -88,6 +89,7 @@ get_input:
     pop edx
     pop ebx
     pop ecx
+    pop eax
     ret
 
 ;Move CURSOR forward(need correction with the limits in case of bad assumptions!!!)
@@ -107,9 +109,8 @@ adv_cursor:
     loop .loop1
   ;While CURSOR is out of screen, advance SCREEN_START
   .loop2:
-    mov ebx, SCREEN_START
-    add ebx, 1920
-    cmp ebx, eax
+    mov ebx, SCREEN_START  
+    cmp 1920, eax
     ja .end2;If CURSOR is on screen, end.
     ;Else advance SCREEN_START and adjust CURSOR.
     add ebx, 80
