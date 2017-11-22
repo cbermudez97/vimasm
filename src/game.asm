035f0e6f87b944c27888ed663bb57ef46ab7d5b6
Carlos Bermudez Porto <cbermu.97@gmail.com>
2017-11-21 21:16:27 -0500

    Changes made to game.asm. Now works correctly.

--- src/game.asm
+++ src/game.asm
@@ -5,8 +5,9 @@ section .data
 ASCII_CODE_NS db  0,27,49,50,51,52,53,54,55,56,57,48,45,61,8,9,113,119,101,114,116,121,117,105,111,112,91 ,93 ,13,0,97,115,100,102,103,104,106,107,108,59,39,96 ,0,92 ,122,120,99,118,98,110,109,44,46,47,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
 ASCII_CODE_S  db  0,27,33,64,35,36,37,94,38,42,40,41,95,43,8,9,81 ,87 ,69 ,82 ,84 ,89 ,85 ,73 ,79 ,80 ,123,125,13,0,65,83 ,68 ,70 ,71 ,72 ,74 ,75 ,76 ,58,34,126,0,124,90 ,88 ,67,86 ,66,78 ,77 ,60,62,63,0,0,0,32,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,45,0,0,0,43,0,0,0,0,0
 ASCII_CODE_LEN dd 83
+
 ;Data related to the text and cursor.
-TEXT times 1000000 db 0
+TEXT times 10000000 db 0
 SCREEN_START dd 0
 CURSOR dd 0
 
@@ -47,16 +48,15 @@ game:
   FILL_SCREEN BG.BLACK
   ; Calibrate the timing
   call calibrate
-
   ; Snakasm main loop
   game.loop:
     .input:
       call get_input
     ; Main loop.
     jmp game.loop
+    ret
 
 get_input:
-    push eax
     call scan; Hex code in eax(al)
     ; Check if value is valid
     cmp al, 0
@@ -66,7 +66,7 @@ get_input:
     push edx
     ; Check for bindings.(enter,backspace...)
     ;Binds needed.
-
+    
     ;Update the the text if char.
       .noShift:    
       GET_ASCII_NS eax, bl
@@ -86,7 +86,6 @@ get_input:
     pop edx
     pop ebx
     pop ecx
-    pop eax
     ret
 
 ;Move CURSOR forward(need correction with the limits in case of bad assumptions!!!)
