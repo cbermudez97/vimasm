76eba3a017b90aff7eca3a165f1a5a0e01e02108
Carlos Bermudez Porto <cbermu.97@gmail.com>
2017-12-07 16:58:32 -0500

    Now the presentation calls the insertion method when a key is pressed.

--- src/insertion.asm
+++ src/insertion.asm
@@ -65,6 +65,8 @@ insertion:
   mov byte [TEXT], 3
   mov dword [END], TEXT
   ; Insertion mode main loop
+  ; Temp seting the cursor to zero
+  mov dword [CURSOR], 0
   .loop:
     .input:
       ;Cleaning registries.
